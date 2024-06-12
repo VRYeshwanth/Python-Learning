cdef2c29b0439040fccb2a26ab2074608feaab97
VR Yeshwanth <124450398+VRYeshwanth@users.noreply.github.com>
2024-06-12 16:56:18 +0530

    KEA Website Checker
    This script opens the KEA website and checks for any latest announcements

---
+++ KEA_Checker.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+from datetime import date
+
+#These are date formats used to check the announcement
+def format_1():
+    d = date.today()
+    fd = d.strftime("%d-%m")
+    return fd
+def format_2():
+    d = date.today()
+    fd = d.strftime("%d/%m/%Y")
+    return fd
+
+url = "https://cetonline.karnataka.gov.in/kea/"
+browser = webdriver.Chrome()
+browser.get(url)
+browser.maximize_window()
+sleep(2)
+btn = browser.find_element(By.XPATH, '//*[@id="ddlLanguage"]/option[2]').click()
+sleep(2)
+browser.execute_script("window.scrollBy(0,880)", "")
+table = browser.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_Gridlatestannoc"]')
+
+#This part of the code scrapes the announcements of the current date and prints them
+todays_announcements = []
+tr_tags = table.find_elements(By.TAG_NAME, 'tr')
+for tr in tr_tags:
+    text = tr.text.strip()
+    if format_1() in text or format_2() in text:
+        todays_announcements.append(text)
+print("Today's Announcements")
+for i, announcement in enumerate(todays_announcements):
+    print(f"{i+1}) {announcement}")
+sleep(5)

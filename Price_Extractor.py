46a4e6cbb88cd179c93431fb7a5155a546e3f113
VR Yeshwanth <124450398+VRYeshwanth@users.noreply.github.com>
2024-06-13 10:52:41 +0530

    Updated row selection
    The row selection issue is sorted

--- Price_Extractor.py
+++ Price_Extractor.py
@@ -2,39 +2,40 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 Names = []
 Prices = []
 Ratings = []
 
-for i in range(2):
+for i in range(10):
+    sleep(2)
     url = f"https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={i+1}"
     browser = webdriver.Chrome()
     browser.get(url)
     sleep(2)
     browser.maximize_window()
     soup = BeautifulSoup(browser.page_source, "html.parser")
-    sleep(1)
-    browser.quit()
+    sleep(2)
     table = soup.find("div", class_ = "DOjaWF gdgoEp")
     rows = table.find_all("div", class_ = "cPHDOP col-12-12")
-    l = len(rows)-2
+    if i == 0:
+        l = len(rows)-2
+    else:
+        l = len(rows)-1
     rows = rows[:l]
-
+    sleep(2)
     for row in rows:
-        name = row.find("div", class_ = "KzDlHZ").text.strip()
-        price = row.find("div", class_ = "Nx9bqj _4b5DiR").text.strip()
+        name = row.find("div", class_ = "KzDlHZ")
+        price = row.find("div", class_ = "Nx9bqj _4b5DiR")
         rating = row.find("div", class_ = "XQDdHH")
-        
-        Names.append(name)
-        Prices.append(price)
-        if rating:
-            Ratings.append(rating.text.strip())
+        if name == None:
+            continue
         else:
-            Ratings.append("-")
+            Names.append(name.text.strip() if name else "None")
+            Prices.append(price.text.strip() if price else "None")
+            Ratings.append(rating.text.strip() if rating else "None")
     print(f"Page {i+1} scraped")
+    browser.quit()
 
 data = {
     "Name of Product": Names,
@@ -42,4 +43,5 @@ data = {
     "Rating": Ratings
 }
 df = pd.DataFrame(data)
+print(df)
 df.to_excel("Laptop Prices.xlsx")

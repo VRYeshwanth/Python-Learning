29c240e805fb2c4433b37d55e231e0734f09917c
VR Yeshwanth <124450398+VRYeshwanth@users.noreply.github.com>
2024-06-13 09:56:15 +0530

    Used selenium
    Incorporated selenium in the project. Need to update the row selection

--- Price_Extractor.py
+++ Price_Extractor.py
@@ -1,38 +1,45 @@
 from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import openpyxl
-import time
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 Names = []
 Prices = []
 Ratings = []
 
-for i in range(1,21):
-    url = f"https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={i}"
+for i in range(2):
+    url = f"https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page={i+1}"
+    browser = webdriver.Chrome()
+    browser.get(url)
+    sleep(2)
+    browser.maximize_window()
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    sleep(1)
+    browser.quit()
+    table = soup.find("div", class_ = "DOjaWF gdgoEp")
+    rows = table.find_all("div", class_ = "cPHDOP col-12-12")
+    l = len(rows)-2
+    rows = rows[:l]
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-
-    main_div = soup.find("div", class_ = "DOjaWF gdgoEp")
-    data_div = main_div.find_all("div", class_ = "yKfJKb row")
-    for i in data_div:
-            name_div = i.find("div", class_ = "KzDlHZ")
-            rating_div = i.find("div", class_ = "XQDdHH")
-            price_div = i.find("div", class_ = "Nx9bqj _4b5DiR")
-
-            Names.append(name_div.text.strip() if name_div else None)
-            Prices.append(price_div.text.strip() if price_div else None)
-            Ratings.append(rating_div.text.strip() if rating_div else None)
-    
-    time.sleep(2)
+    for row in rows:
+        name = row.find("div", class_ = "KzDlHZ").text.strip()
+        price = row.find("div", class_ = "Nx9bqj _4b5DiR").text.strip()
+        rating = row.find("div", class_ = "XQDdHH")
+        
+        Names.append(name)
+        Prices.append(price)
+        if rating:
+            Ratings.append(rating.text.strip())
+        else:
+            Ratings.append("-")
+    print(f"Page {i+1} scraped")
 
 data = {
     "Name of Product": Names,
     "Price": Prices,
     "Rating": Ratings
 }
-
 df = pd.DataFrame(data)
-print(df)
-df.to_excel("Laptop_Prices.xlsx")
+df.to_excel("Laptop Prices.xlsx")
